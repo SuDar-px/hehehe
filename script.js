5906cb09213ef7f5be58db99c0c82ffa3d3da59a
SuDar-px <152791061+SuDar-px@users.noreply.github.com>
2024-04-11 23:19:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+  const audio = document.querySelector('audio');
+  const body = document.body;
+
+  audio.addEventListener('play', () => {
+    body.classList.add('play-music');
+  });
+
+  audio.addEventListener('pause', () => {
+    body.classList.remove('play-music');
+  });
